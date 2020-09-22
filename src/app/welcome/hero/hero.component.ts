20343144392576c19dae4395577b976ff41ea869
mitsui <m.mihye1128@gmail.com>
2020-09-22 16:36:34 +0900

    feat(welcome)heroコンポーネント作成

---
+++ src/app/welcome/hero/hero.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent implements OnInit {
+  @Input() productName: string;
+  @Input() limitTime: number;
+
+  user$ = this.authService.afUser$.pipe(tap(() => (this.isLoading = false)));
+  isLoading: boolean;
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.loginProcessing = true;
+    this.authService.login().finally(() => {
+      this.authService.loginProcessing = false;
+    });
+  }
+}
