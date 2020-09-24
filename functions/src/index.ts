b29e6d984aa123254309518e0c5362113fa7fdd1
komura <affirman03139@outlook.jp>
2020-09-25 05:59:01 +0900

    feat(tweet):一旦tweetのfunctionsを実装、tweet,reTweet関数実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,10 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
+export {
+  checkTaskComplete,
+  checkTaskCompleteTest
+} from './twitter.function';
+export {
+  saveTweetData,
+} from './task.function';
