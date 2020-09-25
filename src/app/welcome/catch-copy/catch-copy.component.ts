3f617affc233d003803ec40b803f505a03cb13c6
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-09-25 23:24:51 +0900

    feat(catch-copy): キャッチコピーコンポーネント作成

---
+++ src/app/welcome/catch-copy/catch-copy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catch-copy',
+  templateUrl: './catch-copy.component.html',
+  styleUrls: ['./catch-copy.component.scss']
+})
+export class CatchCopyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -6,9 +6,10 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { MatCardModule } from '@angular/material/card';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
+import { CatchCopyComponent } from './catch-copy/catch-copy.component';
 
 @NgModule({
-  declarations: [WelcomeComponent, HeroComponent, AboutComponent],
+  declarations: [WelcomeComponent, HeroComponent, AboutComponent, CatchCopyComponent],
   imports: [CommonModule, WelcomeRoutingModule, MatCardModule],
 })
 export class WelcomeModule {}
