0b219f64310186c2170c74c64b799a4eb0528829
mitsui <m.mihye1128@gmail.com>
2020-09-26 15:06:56 +0900

    fix(welcome):サービス名決定

--- src/app/welcome/catch-copy/catch-copy.component.ts
+++ src/app/welcome/catch-copy/catch-copy.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-catch-copy',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catch-copy.component.scss'],
 })
 export class CatchCopyComponent implements OnInit {
+  @Input() productName: string;
+
   constructor() {}
 
   ngOnInit(): void {}
